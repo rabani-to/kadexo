60c787d2963af9dc9e43a753d4d97c024ffd055c
D3Portillo <d3portillo@gmail.com>
2025-04-16 17:29:31 +0200

    chores: init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Next 14 - World Mini App Template
+
+This template was generated with [create-world-app](https://www.npmjs.com/package/create-world-app). Fasten your develpoment on [World Mini Apps](https://docs.world.org/mini-apps) with ready-to-use Auth, hooks and ready UI components with helpers.
+
+## Installation
+
+```sh
+npm install
+```
+
+## Start development
+
+```sh
+npm run dev
+```
+
+## Teck stack
+
+- Auth: [@radish-la/world-auth](https://www.npmjs.com/package/@radish-la/world-auth)
+- UI Library: [World UI kit](https://mini-apps-ui-kit.world.org/?path=/docs/components-numberpad--docs)
+- Custom UI Components: [shadcn/ui](https://ui.shadcn.com/docs/components/sonner)
+- Styling: [TailwindCSS](https://tailwindcss.com/docs/styling-with-utility-classes)
+- State Management: [Jotai](https://jotai.org/)
+- React Framework: [Next14](https://nextjs.org/docs/14/getting-started)
+- Debug Console: [Eruda](https://eruda.liriliri.io/)
---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { useWorldAuth } from "@radish-la/world-auth"
+import Brr from "./Brr"
+
+export default function Home() {
+  const { user, isMiniApp, isConnected, signIn } = useWorldAuth({
+    onWrongEnvironment() {
+      // something to do when minikit is not installed
+      alert("Hey. This only works inside World App")
+    },
+  })
+
+  return (
+    <main className="flex min-h-screen gap-2 flex-col items-center justify-center p-10">
+      <h1 className="text-4xl text-center font-bold">You've been World'd!</h1>
+      <p className="text-lg opacity-70 max-w-xl text-center">
+        Welcome {user?.username ? <strong>{user.username}</strong> : ""} to the
+        beginning of your Worldcoin journey. This is a template for building
+        mini-apps with Auth and UI ready to go <Brr />
+      </p>
+      <button
+        className="bg-black mt-4 mx-auto font-semibold text-xl max-w-xs w-full text-white px-6 py-4 rounded-full"
+        onClick={signIn}
+      >
+        Connect Wallet
+      </button>
+    </main>
+  )
+}
