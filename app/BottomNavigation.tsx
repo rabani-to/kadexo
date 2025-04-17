01ef760d74756a0d7bb1002135946165b057fc35
D3Portillo <d3portillo@gmail.com>
2025-04-17 16:40:09 +0200

    fix(BottomNavigation): force bg + default to market

--- app/BottomNavigation.tsx
+++ app/BottomNavigation.tsx
@@ -9,13 +9,13 @@ import { TiHome } from "react-icons/ti"
 
 export default function BottomNavigation() {
   return (
-    <Tabs asChild>
+    <Tabs defaultValue="market" asChild>
       <Fragment>
         <TabsList asChild>
-          <nav className="w-full sticky bottom-0 bg-black/90 border-t border-black backdrop-blur rounded-none h-auto grid grid-cols-4">
+          <nav className="w-full sticky bottom-0 !bg-black/90 border-t border-black backdrop-blur rounded-none h-auto grid grid-cols-4">
             <TabsTrigger
               className="grid place-items-center p-1.5 h-12 rounded-none text-white !bg-transparent text-white/40 data-[state=active]:text-yellow-400"
-              value="buy"
+              value="market"
             >
               <div className="size-5 grid place-items-center">
                 <TiHome className="text-xl" />
